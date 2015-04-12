13e016ad0d048dfba4a3a7a174e364154bfabac8
unknown <guru.bobbi@gmail.com>
2015-04-11 22:31:27 -0400

    read files

--- src/test/java/com/playground/testsuite/IndicatorTest.java
+++ src/test/java/com/playground/testsuite/IndicatorTest.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.playground.FileReader;
+import com.playground.model.Ticker;
+import com.playground.service.FileReaderService;
 /**
  * Unit test for simple App.
  */
@@ -17,12 +18,23 @@ public class IndicatorTest {
 	
 	@Test
     public void doFilesTest() throws Exception {
-    	FileReader reader = new FileReader();
+		FileReaderService reader = new FileReaderService();
     	reader.setDirectoryName("C:\\Vault\\bhav");
     	List<File> files = reader.loadDataFiles();
     	
     	for(File file: files) {
     		System.out.println(file.toString());
     	}    	
-    }	
+    }
+	
+	@Test
+	public void doReadFilesTest() throws Exception {
+		FileReaderService reader =  new FileReaderService();
+    	reader.setDirectoryName("C:\\Vault\\bhav");
+    	List<File> files = reader.loadDataFiles();
+    	List<Ticker> tickers = reader.readDataFiles();
+    	for(Ticker t : tickers) {
+    		System.out.println(t);
+    	}
+	}
 }
